7bbce2b9175ecf3cc39abd4daeb6f310f2b9110b
Ash McKenzie <ash.mckenzie@playup.com>
2011-09-27 19:48:34 +1000

    more refactoring

--- lib/episode.rb
+++ lib/episode.rb
@@ -2,7 +2,7 @@ class WarningException < Exception
 end
 
 class Episode
-  attr_reader :show, :season, :episode, :title
+  attr_reader :show, :season, :series_id, :episode, :title
   
   def initialize path
     @rename = Rename.instance
@@ -27,16 +27,16 @@ class Episode
 
     $log.debug "@file         =[#{@file}]"
     $log.debug "suggested_file=[#{suggested_file}]"
-    $log.debug "#{@file.to_s != suggested_file.to_s}"
+    $log.debug "@file.to_s == suggested_file.to_s = #{@file.to_s == suggested_file.to_s}"
 
-    if same_file?(@file, suggested_file)
+    unless same_file?(@file, suggested_file)
       $log.info "Renaming '#{@file.basename} to '#{suggested_file.basename}"
       rename @file, suggested_file if forreal
     else
       $log.info "Has correct filename '#{@file}'"
     end
-
-    true
+    
+    $log.debug ''
   end
   
   private
@@ -57,20 +57,17 @@ class Episode
     @episode = match[3]
     @extension = match[4].downcase
 
-    $log.debug "Looking for show '#{show}'"
-
     lookup_show
     lookup_series
     lookup_episode
 
     @title = @rename.episodes["#{show}-#{season}-#{episode}"].name
-
-    true
   end
   
   def lookup_show
     show_cache_file = "#{show}/show.marshal"
     unless (@rename.shows[show] ||= read_cache(show_cache_file))
+      $log.debug "No cache for show '#{show}'"
       @rename.shows[show] = @tv_db.search(show).first
       cache show_cache_file, @rename.shows[show]
     else
@@ -79,11 +76,12 @@ class Episode
   end
 
   def lookup_series
-    series_id = @rename.shows[show]["seriesid"]
+    @series_id = @rename.shows[show]["seriesid"]
     series_cache_file = "#{show}/series_#{season}/series.marshal"
-    unless (@rename.series[series_id] ||= read_cache(series_cache_file))
-      @rename.series[series_id] = @tv_db.get_series_by_id(series_id)
-      cache series_cache_file, @rename.series[series_id]
+    unless (@rename.series[@series_id] ||= read_cache(series_cache_file))
+      $log.debug "No cache for show '#{show}', season '#{season}'"
+      @rename.series[@series_id] = @tv_db.get_series_by_id(@series_id)
+      cache series_cache_file, @rename.series[@series_id]
     else
       $log.debug "Cache hit for show '#{show}', season '#{season}'"
     end
@@ -92,7 +90,8 @@ class Episode
   def lookup_episode
     episode_cache_file = "#{show}/series_#{season}/episode_#{episode}.marshal"
     unless (@rename.episodes["#{show}-#{season}-#{episode}"] ||= read_cache(episode_cache_file))
-      if ep = @rename.series[series_id].get_episode(season.to_i, episode.to_i)
+      $log.debug "No cache for show '#{show}', season '#{season}', episode '#{episode}'"
+      if ep = @rename.series[@series_id].get_episode(season.to_i, episode.to_i)
         @rename.episodes["#{show}-#{season}-#{episode}"] = ep
         cache episode_cache_file, ep
       else
@@ -105,7 +104,6 @@ class Episode
   
   def read_cache file
     file = "./cache/#{file}"
-    $log.debug "Attemping to read cache from '#{file}"
     return false unless File.exist? file
     Marshal.load File.read(file)
   end
@@ -120,7 +118,7 @@ class Episode
   end
 
   def same_file? file1, file2
-    @file.to_s.downcase != suggested_file.to_s.downcase
+    @file.to_s.downcase == suggested_file.to_s.downcase
   end
 
   def suggested_file
